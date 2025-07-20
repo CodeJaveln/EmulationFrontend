4af1adff70d5177b2bb46c7c1d431b695143371e
CodeJaveln <123097758+CodeJaveln@users.noreply.github.com>
2025-07-20 16:39:54 +0200

    Raylib test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
+#include <raylib.h>
+
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 450;
+
 int main(void) {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello, World!");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
